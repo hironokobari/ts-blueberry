5e5f8c168df7574dd8ce5dbdb4873b2f5a45f1e2
hironokobari <kobari0127@outlook.jp>
2024-12-26 23:23:00 +0900

    よく使う型について

--- src/chap1/index.js
+++ src/chap1/index.js
@@ -1,7 +1,43 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * number型
+ */
 const binary = 0b1010; // 2進数リテラル
 const octal = 0o755; // 8進数リテラル
 const hexadecimal = 0xff; // 16進数リテラル
 console.log(binary, octal, hexadecimal); // 10 493 255
+/**
+ * bigint型：任意精度（どのくらい大きい数であっても正しく表示できる）
+ */
 const bigNum = (123n + 456n) * 2n;
 console.log(bigNum);
+/**
+ * Unicodeを使用
+ */
+console.log('Hello \u{796d} world!'); // Hello 祭 world!
+/**
+ * nullとundefined
+ * 何もないことを示すプリミティブが2種類存在するのはjs,tsの特徴
+ */
+const val1 = null;
+const val2 = undefined; // undefinedの方がサポートが手厚い
+/**
+ * 暗黙の変換①
+ */
+const readline_1 = require("readline");
+const rl = (0, readline_1.createInterface)({
+    input: process.stdin,
+    output: process.stdout,
+});
+// rl.question('数字を入力してください（string型で処理されます）:', (num1) => {
+//   const sum1 = num1 + 1000; // string型に暗黙の変換が行われている
+//   console.log(sum1);
+//   rl.close;
+// });
+rl.question('数字を入力してください（number型で処理されます）:', (num1) => {
+    const num2 = Number(num1); // number型に変換
+    const sum1 = num2 + 1000;
+    console.log(sum1);
+    rl.close;
+});
--- src/chap1/index.ts
+++ src/chap1/index.ts
@@ -1,8 +1,52 @@
+/**
+ * number型
+ */
 const binary = 0b1010; // 2進数リテラル
 const octal = 0o755; // 8進数リテラル
 const hexadecimal = 0xff; // 16進数リテラル
 
 console.log(binary, octal, hexadecimal); // 10 493 255
 
+/**
+ * bigint型：任意精度（どのくらい大きい数であっても正しく表示できる）
+ */
 const bigNum: bigint = (123n + 456n) * 2n;
 console.log(bigNum);
+
+/**
+ * Unicodeを使用
+ */
+console.log('Hello \u{796d} world!'); // Hello 祭 world!
+
+/**
+ * nullとundefined
+ * 何もないことを示すプリミティブが2種類存在するのはjs,tsの特徴
+ */
+const val1: null = null;
+const val2: undefined = undefined; // undefinedの方がサポートが手厚い
+
+/**
+ * 暗黙の変換
+ */
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// rl.question('数字を入力してください（string型で処理されます）:', (num1) => {
+//   const sum1 = num1 + 1000; // string型に暗黙の変換が行われている
+//   console.log(sum1);
+//   rl.close;
+// });
+
+rl.question('数字を入力してください（number型で処理されます）:', (num1) => {
+  const num2 = Number(num1); // number型に変換
+  /**
+   * Numberで変換できなかった場合NaNが返却される
+   */
+  const sum1 = num2 + 1000;
+  console.log(sum1);
+  rl.close;
+});
